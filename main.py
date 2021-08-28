31d60e5fc27c0493c5a4865d291d781cec7becc4
Mazen Mohamed <85415062+Mazen72@users.noreply.github.com>
2021-08-28 22:11:05 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import pandas as pd
-import numpy as np
-print('no fuckin errors')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
